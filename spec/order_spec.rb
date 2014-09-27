7a20a382e05add4f7b2206eb5b3109667bba22dd
Denise <yu.denise.d@gmail.com>
2014-09-27 21:17:27 +0100

    tests are passing. time for wine

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -7,12 +7,14 @@ describe Order do
 	let(:order) { Order.new }
 	let(:line_item) { LineItem.new }
 
-	it "should sum up two subtotals" do 
+	it "should sum up three subtotals" do 
 		line1 = line_item.subtotal("krabby patty", 2)
 		line2 = line_item.subtotal("salty sea dog", 3)
+		line3 = line_item.subtotal("kelp rings", 1)
 		order.add(line1)
 		order.add(line2)
-		expect(order.grand_total).to eq 6.25
+		order.add(line3)
+		expect(order.grand_total).to eq 7.75
 	end
 	
 end
